79003c4bdc1b401cca0ba326ddb834bfec2e977d
sonjailic <sonjailic94@hotmail.com>
2022-09-20 18:16:07 -0400

    add category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,24 +9,39 @@ router.get('/', (req, res) => {
   Category.findAll({
     include: [Product]
   }).then(data => res.status(200).json(data))
-
+    .catch(err => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: [Product],
+    where: { id: req.params.id }
+  }).then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    req.body,
+    { where: { id: req.params.id } }).then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy(
+    { where: { id: req.params.id } }
+  ).then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
